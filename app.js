91bfd130adbf704c6d9c6a40338c7a2cf2ab8c5b
applefrittr <jacob.lee.jordan@gmail.com>
2023-07-03 12:26:39 -0400

    Changes required for deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const compression = require("compress");
+const compression = require("compression");
 const helmet = require("helmet");
 
 const indexRouter = require("./routes/index");
